baacbdae467b9d4f815d98ad35fceb4d640e04ac
VladKhorev <vladik1crut@gmail.com>
2024-12-23 18:38:06 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+username = "Владислав"
+title = "Заметка о проекте"
+content = "Описание проекта, который нужно завершить к дедлайну."
+status = "В процессе"
+created_date = "23-12-2024"
+issue_date = "23-12-2024"
+
+
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
