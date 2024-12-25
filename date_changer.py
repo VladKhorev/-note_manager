88c543bc008607958431f1fd4b4d32d747aab85c
VladKhorev <vladik1crut@gmail.com>
2024-12-25 18:22:14 +0300

    date_changer.py
    Grade 1. Этап 1: Задание 2

--- date_changer.py
+++ date_changer.py
@@ -1,1 +1,19 @@
-Grade 1. Этап 1: Задание 2
+username = "Владислав"
+title = "Заметка о проекте"
+content = "Описание проекта, который нужно завершить к дедлайну."
+status = "В процессе"
+created_date = "23-12-2024"
+issue_date = "23-12-2024"
+temp_created_date = created_date[:-5]
+temp_issue_date = issue_date[:-5]
+created_date = input("Введите дату создания заметки (дд-мм-гггг): ")
+issue_date = input("Введите дату истечения заметки (дд-мм-гггг): ")
+
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
+print("Дата создания заметки:", temp_created_date)
+print("Дата истечения заметки:", temp_issue_date)
